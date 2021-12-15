3d36e3e71cf93a78701cdb1aa5feb6eb72818052
designeralabi <bdlkrmalabi@gmail.com>
2021-12-14 20:32:22 -0800

    sidebar functionity added

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ const sidebar = document.querySelector(".side-bar");
 
 
 hamburger.addEventListener("click", ()=>{
-    
+    console.log("you clicked me");
+
+    sidebar.classList.toggle ("change-sidebar");
 });
