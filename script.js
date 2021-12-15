a6119cceba626fc6fa0fff80f1e0095b98c4e91b
designeralabi <bdlkrmalabi@gmail.com>
2021-12-15 13:50:04 -0800

    broken link fix

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const hamburger = document.querySelector(".hamburger-menu");
 
 const sidebar = document.querySelector(".side-bar");
 
+const yearOutput = document.querySelector("#yearOutput");
+
+// fetch the date object
+const nowDate = new Date();
+// fet the currentYear from nowDate 
+yearOutput.textContent = nowDate.getFullYear() + ",";
 
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("change-hamburger");
     sidebar.classList.toggle ("change-sidebar");
 });
+
